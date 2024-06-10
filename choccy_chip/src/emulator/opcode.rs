9c7f2793dcb9aee5a657c4498f7a7f8d092e7453
SeniorMars <cjh16@rice.edu>
2024-06-10 16:07:18 -0400

    attempted to code up some opcodes + graphics trait

--- choccy_chip/src/emulator/opcode.rs
+++ choccy_chip/src/emulator/opcode.rs
@@ -12,7 +12,10 @@ type Register = u8; // a 4 bit register number
 #[derive(Debug, PartialEq)]
 pub(crate) enum OpCode {
     Nop,
-    Call(Address),
+    Display(Option<(Constant, Constant, Constant)>), // Whether to clear or draw
+    Return,                                          // NOTE: technically a flow control instruction
+    Call(Address),                                   // NOTE: This is deprecated
+    Flow(Case, Address),
     BitOp((Register, Register, Case)),
     Unknown,
 }
@@ -28,7 +31,17 @@ impl From<u16> for OpCode {
 
         match digits {
             (0, 0, 0, 0) => OpCode::Nop,
+            (0, 0, 0xE, 0) => OpCode::Display(None),
+            (0, 0, 0xE, 0xE) => OpCode::Return, // technically a flow control instruction
             (0, _, _, _) => OpCode::Call(value & 0x0FFF), // Get rid of the first digit
+            (1 | 2 | 0xB, _, _, _) => {
+                let flow_case = u8::try_from(digits.0).expect("Invalid flow case");
+
+                // let address = (digits.1 << 8) | digits.2 << 4 | digits.3;
+                let address = value & 0x0FFF;
+
+                OpCode::Flow(flow_case, address)
+            }
             (8, register_x, register_y, constant) => {
                 let args = (
                     u8::try_from(register_x).expect("Invalid register number"),
@@ -36,6 +49,14 @@ impl From<u16> for OpCode {
                     u8::try_from(constant).expect("Invalid constant"),
                 );
                 OpCode::BitOp(args)
+            }
+            (0xD, x_pos, y_pos, constant) => {
+                let args = (
+                    u8::try_from(x_pos).expect("Invalid register number"),
+                    u8::try_from(y_pos).expect("Invalid register number"),
+                    u8::try_from(constant).expect("Invalid constant"),
+                );
+                OpCode::Display(Some(args))
             }
             _ => OpCode::Unknown,
         }
@@ -54,8 +75,6 @@ impl Emu {
         let opcode = (higher_byte << 8) | lower_byte;
 
         // increment the program counter by 2
-        // NOTE: should this function just be responsible for fetching the opcode?
-        // i.e., should we have a generic fetch function that increments the program counter too?
         self.psuedo_registers.program_counter += 2;
 
         OpCode::from(opcode)
@@ -69,9 +88,48 @@ impl Emu {
     pub(crate) fn execute_opcode(&mut self, opcode: &OpCode) {
         match opcode {
             OpCode::Nop => {}
-            &OpCode::Call(address) => todo!(),
+            OpCode::Call(_) => panic!("DEPRECATED!"), // NOTE: deprecated!
+            OpCode::Display(_to_draw) => todo!(),
+            OpCode::Return => self.handle_return(), // NOTE: technically a flow instruction
+            OpCode::Flow(case, address) => self.handle_flow(*case, *address),
             OpCode::BitOp(args) => self.handle_bit_op(*args),
             OpCode::Unknown => unreachable!(),
+        }
+    }
+
+    /// Handle a return instruction from a subroutine.
+    ///
+    /// The interpreter sets the program counter to the address at the top of the stack, then
+    /// subtracts 1 from the stack pointer.
+    fn handle_return(&mut self) {
+        let return_address = self.pop_stack();
+        self.set_program_counter(return_address);
+    }
+
+    /// Handle a flow instruction.
+    ///
+    /// # Arguments
+    /// - `case`: The case to switch on.
+    /// - `address`: The address where the flow instruction is acting upon.
+    ///
+    /// # Cases
+    /// - 1: Jump (GOTO) to the address given.
+    /// - 2: Call subroutine at the address given.
+    /// - B or 11: Jumps to the address nnn plus V0.
+    fn handle_flow(&mut self, case: Case, address: Address) {
+        match case {
+            //  The interpreter sets the program counter to nnn.
+            1 => self.set_program_counter(address),
+            //  The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
+            2 => {
+                self.push_stack(self.program_counter());
+                self.set_program_counter(address); // what now? KINDA confused
+            }
+            11 => {
+                let v0 = u16::from(self.get_register_val(0));
+                self.set_program_counter(address + v0);
+            }
+            _ => todo!(),
         }
     }
 
@@ -84,15 +142,120 @@ impl Emu {
 mod tests {
     use super::*;
 
-    #[test]
-    #[should_panic = "assertion `left == right` failed"]
-    fn test_fetch_opcode() {
+    fn setup() -> Emu {
         let mut emu = Emu::new();
-        emu.ram[0] = 0x12;
+        emu.psuedo_registers.program_counter = 0; // just so we start with the same state
+        emu
+    }
+
+    #[test]
+    fn test_opcode_nop() {
+        let mut emu = setup();
+        emu.ram[0] = 0x00;
+        emu.ram[1] = 0x00;
+
+        let opcode = emu.fetch_opcode();
+        assert_eq!(opcode, OpCode::Nop);
+    }
+
+    #[test]
+    #[should_panic = "DEPRECATED!"]
+    fn test_opcode_call() {
+        let mut emu = setup();
+
+        emu.ram[0] = 0x02;
         emu.ram[1] = 0x34;
 
         let opcode = emu.fetch_opcode();
 
         assert_eq!(opcode, OpCode::Call(0x234));
+
+        emu.execute_opcode(&opcode);
+    }
+
+    #[test]
+    #[should_panic = "not yet implemented"]
+    fn test_opcode_display() {
+        let mut emu = setup();
+
+        emu.ram[0] = 0xD0;
+        emu.ram[1] = 0x00;
+
+        let opcode = emu.fetch_opcode();
+
+        assert_eq!(opcode, OpCode::Display(Some((0, 0, 0))));
+
+        emu.execute_opcode(&opcode);
+    }
+
+    #[test]
+    fn test_opcode_return() {
+        let mut emu = setup();
+
+        emu.push_stack(0x200);
+
+        emu.ram[0] = 0x00;
+        emu.ram[1] = 0xEE;
+
+        let opcode = emu.fetch_opcode();
+
+        assert_eq!(opcode, OpCode::Return);
+
+        emu.execute_opcode(&opcode);
+
+        assert_eq!(emu.psuedo_registers.program_counter, 0x200);
+    }
+
+    #[test]
+    fn test_opcode_flow_jump() {
+        let mut emu = setup();
+
+        emu.ram[0] = 0x12;
+        emu.ram[1] = 0x34;
+
+        let opcode = emu.fetch_opcode();
+
+        assert_eq!(opcode, OpCode::Flow(1, 0x234));
+
+        emu.execute_opcode(&opcode);
+
+        assert_eq!(emu.psuedo_registers.program_counter, 0x234);
+    }
+
+    #[test]
+    fn test_opcode_flow_call() {
+        let mut emu = setup();
+
+        emu.ram[0] = 0x23;
+        emu.ram[1] = 0x45;
+
+        let opcode = emu.fetch_opcode();
+
+        assert_eq!(opcode, OpCode::Flow(2, 0x345));
+
+        emu.execute_opcode(&opcode);
+
+        assert_eq!(emu.psuedo_registers.program_counter, 0x345);
+        let sp = emu.stack_pointer();
+        assert_eq!(sp, 1);
+        assert_eq!(emu.stack[sp as usize], 0);
+    }
+
+    #[test]
+    fn test_opcode_flow_jump_v0() {
+        let mut emu = setup();
+
+        emu.set_register_val(0, 0x12);
+
+        emu.ram[0] = 0xB3;
+        emu.ram[1] = 0x45;
+
+        let opcode = emu.fetch_opcode();
+
+        assert_eq!(opcode, OpCode::Flow(11, 0x345));
+
+        emu.execute_opcode(&opcode);
+
+        assert_eq!(emu.psuedo_registers.program_counter, 0x357);
     }
 }
